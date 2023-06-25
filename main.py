7a2647b0216a30354d1453b7e85ef188b97a9fa8
Ray Han <96783422+RayHan404@users.noreply.github.com>
2023-06-25 09:08:10 +0900

    Coded a program to find the numbers of new iterations with few bugs

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import json
-import pprint
+
+import time
+
+from pprint import pprint
+
 
 with open('data.json', 'r') as f:
   data = json.load(f)
 
+def find_next(prev):
+    pass
+
+for i in range()
+
+pprint(data)
